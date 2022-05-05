4c9f757f570a34d3d11bb9871ed72a3d7fd63826
Vlad <vladislavmartyanov444@gmail.com>
2022-05-05 23:46:46 +0500

    what is wonderful api...

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +24,23 @@ namespace AirQualify
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void SearchButton(object sender, RoutedEventArgs e)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new($"http://api.waqi.info/feed/{SearchBox.Text}/?token=795089feaf4570da25cbe9896ff551e54eaff9bc"),
+            };
+            using (var response =await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                MessageBox.Show(body);
+            }
+
         }
     }
 }
